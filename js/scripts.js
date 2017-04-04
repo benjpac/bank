aa0b72d5daf1ab35bec0f45a117c47719489bac3
Ben Metzger and Dan Jacob <dan@>
2017-04-04 12:01:44 -0700

    add some of the business logic and css

--- js/scripts.js
+++ js/scripts.js
@@ -2,12 +2,39 @@
 //
 // Remember to create a BankAccount constructor and a prototype that includes methods for deposit and withdrawal and any other properties needed. Here is an example wireframe to help you visualize the app:
 
+// if time allows, add:
+// report name + balance with prototype
+// account creation with password
+
+
 //business logic
+function BankAccount(username, initialDeposit) {
+  debugger;
+  this.username = username;
+  this.balance = initialDeposit;
+}
+
+BankAccount.prototype.accountAction = function(withdrawlAmount, depositAmount) {
+  this.balance -= withdrawlAmount;
+  this.balance += depositAmount;
+  return this.balance;
+}
 
 // user interface logic
 $(document).ready(function() {
-  $("#form").submit(function(event) {
+  $("#register").submit(function(event) {
     event.preventDefault();
+    var username = $("#username").val();
+    var initialDeposit = $("#initialDeposit").val();
+    var newAccount = new BankAccount(username, initialDeposit);
+    debugger;
+    $("#balance h3").text(newAccount.balance);
+  });
+  $("#accountAction").submit(function(event) {
+    event.preventDefault();
+    var depositAmount = $("#depositAmount").val();
+    var withdrawlAmount = $("#withdrawlAmount").val();
+    newAccount.accountAction(withdrawlAmount, withdrawlAmount);
 
   });
 });

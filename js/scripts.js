ed80a567b5bbfca89f38a7a2cc7b5a938884ec39
Ben Metzger and Dan Jacob <dan@>
2017-04-04 10:41:40 -0700

    setup initial project files

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// Use constructor functions and prototypes to create a webpage where a user can create a single bank account with an initial deposit amount. Then allow the user to make withdrawals, deposits and see the balance of the account.
+//
+// Remember to create a BankAccount constructor and a prototype that includes methods for deposit and withdrawal and any other properties needed. Here is an example wireframe to help you visualize the app:
+
+//business logic
+
+// user interface logic
+$(document).ready(function() {
+  $("#form").submit(function(event) {
+    event.preventDefault();
+
+  });
+});

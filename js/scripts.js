9cafed5b44d46ca51b450cacb809e85225495623
Ben Metzger and Dan Jacob <ben@>
2017-04-04 16:43:12 -0700

    refactor code

--- js/scripts.js
+++ js/scripts.js
@@ -9,30 +9,24 @@
 var accounts = {};
 //business logic
 function BankAccount(username, initialDeposit) {
-  debugger;
   this.username = username;
-  this.balance = initialDeposit;
+  this.balance = parseFloat(initialDeposit);
+  this.nameBalance = function() {
+    return this.username + ", your balance is $" + this.balance;
+  }
+  this.withdrawal = function(withdrawalAmount) {
+    withdrawalAmount = parseFloat(withdrawalAmount);
+    return this.balance -= withdrawalAmount;
+  }
+  this.deposit = function(depositAmount) {
+    depositAmount = parseFloat(depositAmount);
+    return this.balance += depositAmount;
+  }
 }
 
 function createAccount(username, initialDeposit) {
   accounts[name] = new BankAccount(username, initialDeposit);
   console.log(accounts);
-}
-
-BankAccount.prototype.nameBalance = function() {
-  return this.username + ", your balance is $" + this.balance;
-}
-
-BankAccount.prototype.accountAction = function(depositAmount, withdrawlAmount) {
-  if (!withdrawlAmount) {
-    withdrawlAmount = 0;
-  }
-  if (!depositAmount) {
-    depositAmount = 0;
-  }
-  this.balance -= withdrawlAmount;
-  this.balance += depositAmount;
-  return this.balance;
 }
 
 // user interface logic
@@ -41,16 +35,23 @@ $(document).ready(function() {
     event.preventDefault();
     debugger;
     var username = $("#username").val();
-    var initialDeposit = parseFloat($("#initialDeposit").val());
+    var initialDeposit = $("#initialDeposit").val();
     createAccount(username, initialDeposit);
-    $("#balance").show()
+    $(".balance").show();
+    $(".deposit").show();
+    $(".withdraw").show();
     $("#balance").text(accounts[name].nameBalance());
   })
-  $("#accountAction").submit(function(event) {
+  $(".deposit").submit(function(event) {
     event.preventDefault();
-    var depositAmount = parseFloat($("#depositAmount").val());
-    var withdrawlAmount = parseFloat($("#withdrawlAmount").val());
-    accounts[name].accountAction(depositAmount, withdrawlAmount)
+    var depositAmount = $("#depositAmount").val();
+    accounts[name].deposit(depositAmount);
+    $("#balance").text(accounts[name].nameBalance());
+  });
+  $("#withdraw").submit(function(event) {
+    event.preventDefault();
+    var withdrawAmount = $("#withdrawAmount").val();
+    accounts[name].withdrawal(withdrawAmount);
     $("#balance").text(accounts[name].nameBalance());
   });
 });
